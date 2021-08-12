b86278371c189e74e1ecbc2cc73f325a747a0a8b
michael carini <snownkc@icloud.com>
2021-08-12 12:10:08 +0100

    added EF lesson repo

---
+++ EF_ModelFirst_Starter/EF_ModelFirst/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EF_ModelFirst
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SouthwindContext())
+            {
+
+
+
+            }
+        }
+    }
+}
